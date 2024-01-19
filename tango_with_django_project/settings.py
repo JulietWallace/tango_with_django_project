742893f1f5595f7a338ce1b53895e5223fbdc1e6
Juliet Wallace <2760588w@student.gla.ac.uk>
2024-01-19 09:26:33 +0000

    chapter 4.3 work

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR=os.path.join(BASE_DIR,'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -66,6 +67,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                'django.template.context_processors.media',
+                
             ],
         },
     },
@@ -115,6 +118,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [STATIC_DIR, ]
+
+
+# Media files\
+MEDIA_ROOT=MEDIA_DIR
+MEDIA_URL='/media/'
 
 
 # Default primary key field type
